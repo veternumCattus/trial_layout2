28969bc9237203332fdb740cc6d3df5fd37ce068
veternumCattus <sleepyc.d@gmail.com>
2022-04-24 23:13:32 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/course1/trial_layout2/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.course1.trial_layout2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
